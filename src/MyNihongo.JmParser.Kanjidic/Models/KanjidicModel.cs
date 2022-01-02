01f0f32e69a91753019c5560de50111eae8b33f1
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-01-02 17:18:28 +0200

    add json props

--- src/MyNihongo.JmParser.Kanjidic/Models/KanjidicModel.cs
+++ src/MyNihongo.JmParser.Kanjidic/Models/KanjidicModel.cs
@@ -1,20 +1,30 @@
-﻿namespace MyNihongo.JmParser.Kanjidic.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace MyNihongo.JmParser.Kanjidic.Models;
 
 public sealed record KanjidicModel(int Id)
 {
+	[JsonPropertyName("c")]
 	public char Character { get; internal set; }
 
+	[JsonPropertyName("jlpt")]
 	public byte JlptLevel { get; internal set; }
 
+	[JsonPropertyName("kunYomi")]
 	public IReadOnlyList<string> KunYomi { get; internal set; } = Array.Empty<string>();
 	
+	[JsonPropertyName("onYomi")]
 	public IReadOnlyList<string> OnYomi { get; internal set; } = Array.Empty<string>();
 
+	[JsonPropertyName("en")]
 	public IReadOnlyList<string> English { get; internal set; } = Array.Empty<string>();
 	
+	[JsonPropertyName("fr")]
 	public IReadOnlyList<string> French { get; internal set; } = Array.Empty<string>();
 	
+	[JsonPropertyName("es")]
 	public IReadOnlyList<string> Spanish { get; internal set; } = Array.Empty<string>();
 	
+	[JsonPropertyName("pt")]
 	public IReadOnlyList<string> Portuguese { get; internal set; } = Array.Empty<string>();
 }
